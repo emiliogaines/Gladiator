c3b6a3b7a005563bce6574c3b114faff14189480
moapng <fridell.ml@gmail.com>
2019-12-04 12:11:13 +0100

    added items
    tried to make subclass for items but failed )-:

--- gladiatorspel/Logic.cs
+++ gladiatorspel/Logic.cs
@@ -6,5 +6,14 @@ namespace gladiatorspel
 {
     class Logic
     {
+        public Item[] ListOfItems =
+        {
+            new Item("Helm Of Vitality", 15,5),
+            new Item("Chestpiece of Strength",5,15),
+            new Item("Skullcrusher Helmet", 0, 40)};
+
+
+
+
     }
 }
