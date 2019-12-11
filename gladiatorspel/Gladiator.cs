4e4205fe34b789985bcb33bac485b2fcccde3bd0
Mio <emiliogaiines@gmail.com>
2019-12-11 18:25:22 +0100

    Fixed HP

--- gladiatorspel/Gladiator.cs
+++ gladiatorspel/Gladiator.cs
@@ -26,7 +26,7 @@ namespace gladiatorspel
         public Gladiator(string Name)
         {
             name = Name;
-            baseHealth = 10;
+            baseHealth = 30;
             baseStrength = random.Next(5, 11);
             credits = 0;
         }
